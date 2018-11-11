20e8ccf47f8a612d168252558b7e2a8cc5f84826
github username <github email address>
2018-11-11 10:24:44 -0500

    created article feed

--- src/containers/Articlefeed.js
+++ src/containers/Articlefeed.js
@@ -1,19 +1,43 @@
+// React imports
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import Article from './components/Article'
+
+// Redux imports
+import { connect } from 'react-redux'
+import { fetchArticles } from '../actions/articleActions'
+
 
 class Articlefeed extends Component {
 
   componentDidMount() {
-    
+    this.props.fetchArticles()
   }
 
   render() {
-    return (
 
+    const allArticles = this.props.articles.map((article) => {
+      return (
+        <div key={article.article.id}>
+          <Article
+            title={article.article.title}
+            description={article.article.description}
+            id={article.article.id}
+          />
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        {allArticles}
+      </div>
     )
   }
 
 }
+
+const mapStateToProps = state => ({
+  articles: state.articles.items
+})
 
 export default Articlefeed
