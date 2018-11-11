22c41ec36053108afd1dc2d8297aef0f58ebcdeb
github username <github email address>
2018-11-11 09:52:32 -0500

    created article component and article container. started with article redux

---
+++ src/containers/Articlefeed.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Article from './components/Article'
+
+class Articlefeed extends Component {
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+
+    )
+  }
+
+}
+
+export default Articlefeed
